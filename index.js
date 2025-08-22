2e8dd4e1a023c6f773367136558dd3879a7887bc
Adarsh Sachan <2k23.cs2312635@gmail.com>
2025-08-23 02:27:57 +0530

    updated get request for short url that redirects to original website

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
-const express = require('express');
-const { connectToMongoDB } = require('./connect');
-
+const express = require("express");
+const { connectToMongoDB } = require("./connect");
+const URL = require("./models/url");
 const urlRoute = require("./routes/url");
 
 const app = express();
 const PORT = 8001;
 
-connectToMongoDB('mongodb://localhost:27017/short-url')
-.then(() => console.log('mongodb connected'));
+connectToMongoDB("mongodb://localhost:27017/short-url").then(() =>
+  console.log("mongodb connected")
+);
 
 app.use(express.json());
 
 app.use("/url", urlRoute);
+app.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
+        },
+      },
+    }
+  );
 
-app.listen(PORT, () => {`Server started at ${PORT}`});
+  if (!entry) {
+    return res.status(404).send("Short URL not found");
+  }
 
+  res.redirect(entry.redirectURL);
+});
+
+app.listen(PORT, () => {
+  `Server started at ${PORT}`;
+});
