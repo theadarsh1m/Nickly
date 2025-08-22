a7b7193887a1bc2d9cc67b7e2867f4171a201faf
Adarsh Sachan <2k23.cs2312635@gmail.com>
2025-08-23 02:36:00 +0530

    implemented analytics for url clicks

--- controllers/url.js
+++ controllers/url.js
@@ -14,6 +14,15 @@ async function handleGenerateNewShortUrl(req, res) {
     return res.json({ id: shortId });
 }
 
+async function handleGetAnalytics(req, res) {
+    const shortId = req.params.shortId;
+    const result = await URL.findOne({ shortId });
+    return res.json({ totalClicks:result.visitHistory.length,
+        analytics: result.visitHistory,
+    });
+}
+
 module.exports = {
     handleGenerateNewShortUrl,
+    handleGetAnalytics,
 }
